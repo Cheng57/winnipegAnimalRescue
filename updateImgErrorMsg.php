9203b118051120b32aed724451cef5d46c4e9ad2
Cheng Wu <112827606+Cheng57@users.noreply.github.com>
2023-04-02 21:44:15 -0500

    Error message for the invalid uploaded file
    when the user update the file

---
+++ updateImgErrorMsg.php
@@ -0,0 +1,29 @@
+<?php
+	if (isset($_GET['id'])){
+	    if (filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT)){
+	        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);		
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Listing Post</title>
+	<link rel="stylesheet" href="main.css" type="text/css">
+</head>
+<body>
+
+
+		<div>
+			<h1>An error occured while processing your post.</h1>
+			<p>Please upload an valid image.</p>
+			<a href="edit.php?id=<?=$id ?>">Return edit</a>		
+		</div>
+	
+
+	<div id="footer"> Copywrong 2023 - No Rights Reserved </div>
+</body>
+</html>
