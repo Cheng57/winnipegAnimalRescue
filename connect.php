1ba93ed04cd930a29e6a86e3ae3cd1a50b91bfc9
Cheng Wu <112827606+Cheng57@users.noreply.github.com>
2023-04-02 20:57:56 -0500

    Connect to the database on the server

---
+++ connect.php
@@ -0,0 +1,28 @@
+ <?php
+
+/*******w******** 
+    
+    Name: Cheng Wu
+    Date: 2023-03-18
+    Description: Connect to the database on the server 
+
+****************/
+
+     define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
+     define('DB_USER','serveruser');
+     define('DB_PASS','gorgonzola7!');     
+     
+    //  PDO is PHP Data Objects
+    //  mysqli <-- BAD. 
+    //  PDO <-- GOOD.
+     try {
+         // Try creating new PDO connection to MySQL.
+         $db = new PDO(DB_DSN, DB_USER, DB_PASS);
+         //,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+     } catch (PDOException $e) {
+         print "Error: " . $e->getMessage();
+         die(); // Force execution to stop on errors.
+         // When deploying to production you should handle this
+         // situation more gracefully. ¯\_(ツ)_/¯
+     }
+ ?>
