5d3b1ec0d9a5f55ad16289e8ab217d7613646924
Cheng Wu <cwu57cn@gmail.com>
2023-04-08 12:46:11 -0500

    Create header template
    Created the header template to allow automatically updates applied to all the pages that use the template.

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="main.css" type="text/css">
+	<title>Welcome to Winnipeg Animal Rescue</title>
+</head>
+<body>
+    <div id="wrapper">
+       <div id="header">
+            <h1>
+            	<a href="index.php">Winnipeg Animal Rescue - Index</a>
+        	</h1>           
+       </div>
+       <div id="nav">
+           <ul id="menu">
+               <li>
+                   <a href="index.php" class="active">Home</a>
+               </li>
+               <li>
+                   <a href="post.php">New Post</a>
+               </li>
+               <li>
+                    <a href="index.php">All</a>
+               </li>
+               <li>
+                    <a href="cats.php">Cats</a>
+               </li>
+               <li>
+                    <a href="dogs.php">Dogs</a>
+               </li>
+           </ul>
+       </div>
